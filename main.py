e26428ddcf1a5566739f8d60b937ada4d6b41fb8
Stacey31 <53440572+Stacey-31@users.noreply.github.com>
2021-06-24 00:15:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+####Step2
+dict_food = {"chicken":1.59,
+"Beef":1.99,
+"Cheese": 1.00,
+"Milk":2.50}
+
+
+### Step 3
+Clothes = {"Shirts":1,
+"Sweaters":2,
+"Dresses":3,
+"Shorts":4}
+
+###StepIV
+favorite_clothes = Clothes ["Shirts"]
+
+print(favorite_clothes)
