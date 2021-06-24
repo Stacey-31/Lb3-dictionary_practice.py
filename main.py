2e97191290e602c1b27bf4eb5657263f5ac7909b
Stacey31 <53440572+Stacey-31@users.noreply.github.com>
2021-06-24 22:51:22 +0000

    n

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ dict_grocery = {"chicken":1.59,
 "Cheese":1.00,
 "Milk":2.50}
 #print(dict_grocery)
-chicken_price = dict_grocery ("chicken")
+chicken_price = dict_grocery ["chicken"]
 #print(chicken_price)
+Beef_price = dict_grocery["Beef"]
+#print(Beef_price)
+Cheese_price = dict_grocery["Cheese"]
+#print(Cheese_price)
+Milk_price = dict_grocery["Milk"]
+#print(Milk_price)
 
 ###Step 3
 Clothes = {"Shirts":1,
@@ -18,3 +24,15 @@ favorite_clothes = Clothes ["Shirts"]
 print(favorite_clothes)
 
 #StepIV
+Clothes["Shirts"] = 78 #how we change something in our dictionary
+#print(Clothes)
+
+Clothes["jewelry"] = "5" #how we add something to our dictionary
+print(Clothes)
+
+del Clothes["Sweaters"] #how to delete something to our dictionary
+print(Clothes)
+
+#STEP1
+list_of_cities = ("Oakland","Atlanta","New York City","Seatle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver" )
+list_of_kdramas
