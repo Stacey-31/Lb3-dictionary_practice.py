20b64fa4b37cfd243132f85cc7dac16e695a177a
Stacey31 <53440572+Stacey-31@users.noreply.github.com>
2021-06-24 22:29:32 +0000

    n/a

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 ####Step2
-dict_food = {"chicken":1.59,
+dict_grocery = {"chicken":1.59,
 "Beef":1.99,
-"Cheese": 1.00,
+"Cheese":1.00,
 "Milk":2.50}
+#print(dict_grocery)
+chicken_price = dict_grocery ("chicken")
+#print(chicken_price)
 
-
-### Step 3
+###Step 3
 Clothes = {"Shirts":1,
 "Sweaters":2,
 "Dresses":3,
 "Shorts":4}
 
-###StepIV
+###Step3
 favorite_clothes = Clothes ["Shirts"]
-
 print(favorite_clothes)
+
+#StepIV
