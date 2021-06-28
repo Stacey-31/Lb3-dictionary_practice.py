f0d8248fb63f32894b639033c73e1a731ecfa1a3
Stacey31 <53440572+Stacey-31@users.noreply.github.com>
2021-06-28 22:11:07 +0000

    stuff

--- main.py
+++ main.py
@@ -21,20 +21,33 @@ Clothes = {"Shirts":1,
 
 ###Step3
 favorite_clothes = Clothes ["Shirts"]
-print(favorite_clothes)
+#print(favorite_clothes)
 
 #StepIV
 Clothes["Shirts"] = 78 #how we change something in our dictionary
 #print(Clothes)
 
 Clothes["jewelry"] = "5" #how we add something to our dictionary
-print(Clothes)
+#print(Clothes)
 
 del Clothes["Sweaters"] #how to delete something to our dictionary
-print(Clothes)
+#print(Clothes)
 
 #STEP1
 list_of_cities = ("Oakland","Atlanta","New York City","Seatle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver" )
-print(list_of_cities)
+#print(list_of_cities)
+list_of_cities[2]
+favorite_cities = list_of_cities[5:8]
+print(favorite_cities)
 
-list_of_kdrama = ("Boys over flowers", "Vicenzo", "IOTNBO", "Sky Castle", "Reply 1997", "Mr Queen")
+list_of_kdrama = ("Boys over flowers", "Vicenzo", "IOTNBO", "Sky Castle", "Reply_1997", "Mr Queen", "WWWSK","Reply_1988", "A Korean Odyssey","TWWB")
+#print(list_of_kdrama)
+Mustwatch_kdrama = list_of_kdrama[1:6]
+print(Mustwatch_kdrama)
+
+#StepV
+list_of_cities[0] = "San_Francisco"
+list_of_cities[2] = "Brooklyn"
+list_of_cities[7] = "Hollywood"
+list_of_cities[5] = "Tampa"
+print(list_of_cities)
